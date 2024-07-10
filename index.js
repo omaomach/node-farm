5c1c9594b0ce254d489c1c036ae90192d3460c82
omaomach <aaronjoash44@gmail.com>
2024-07-10 17:44:11 +0300

    worked on 3rd party modules(slugify)

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
+const slugify = require("slugify"); // A slug is basically the last part of a url that contains a unique string that identifies the resource that the website is diplaying
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -32,6 +19,11 @@ const templateProduct = fs.readFileSync(
 const products = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productDataObj = JSON.parse(products);
 console.log(productDataObj);
+
+const slugs = productDataObj.map((el) =>
+  slugify(el.productName, { lower: true })
+);
+console.log(slugs);
 
 const server = http.createServer((request, response) => {
   const { query, pathname } = url.parse(request.url, true);
